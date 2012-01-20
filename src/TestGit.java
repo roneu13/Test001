676fe6dfb4f416bc56d1278869ff558bb30e1f42
Ron <therealron13@gmail.com>
2012-01-20 14:07:44 +0100

    OPEN - task 1: issue 001 http://github.com/roneu13/test001/issues/issue/1
    update

--- src/TestGit.java
+++ src/TestGit.java
@@ -11,7 +11,7 @@ public class TestGit {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("This Git is awesome!");
+		System.out.println("This Git is awesome! That's true.");
 	}
 
 }
